755cbfbafd12de1cb6c12308420f2940ae628ef0
Tusicka <tusicka.iyy@gmail.com>
2017-04-22 18:18:11 +0300

    commit 1

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float speed;
+    private Rigidbody _rb;
+    // Use this for initialization
+    void Start() {
+   _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        _rb.AddForce(direction*speed);
+    }
+}
